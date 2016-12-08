bb3e4bfe36a2a231f06ed9713117f5df1343b93e
Tino Rusch <tino.rusch@webvariants.de>
2016-12-08 09:38:04 +0100

    make it possible to use ecdsa keys;

--- jwt/jwt.go
+++ jwt/jwt.go
@@ -1,6 +1,7 @@
 package jwt
 
 import (
+	"crypto/ecdsa"
 	"crypto/rsa"
 	"errors"
 	"fmt"
@@ -11,18 +12,47 @@ import (
 
 type Claims map[string]interface{}
 
-func CreateToken(claims Claims, key *rsa.PrivateKey) (string, error) {
-	token := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.MapClaims(claims))
-	return token.SignedString(key)
+func CreateToken(claims Claims, key interface{}) (string, error) {
+	switch k := key.(type) {
+	case *rsa.PrivateKey:
+		{
+			token := jwt.NewWithClaims(jwt.SigningMethodRS512, jwt.MapClaims(claims))
+			return token.SignedString(k)
+		}
+	case *ecdsa.PrivateKey:
+		{
+			token := jwt.NewWithClaims(jwt.SigningMethodES512, jwt.MapClaims(claims))
+			return token.SignedString(k)
+		}
+	}
+	return "", errors.New("invalid private key")
 }
 
-func ValidateToken(tokenString string, key *rsa.PublicKey) (Claims, error) {
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
-			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+func ValidateToken(tokenString string, key interface{}) (Claims, error) {
+	var (
+		token *jwt.Token
+		err   error
+	)
+	switch k := key.(type) {
+	case *rsa.PublicKey:
+		{
+			token, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return k, nil
+			})
 		}
-		return key, nil
-	})
+	case *ecdsa.PublicKey:
+		{
+			token, err = jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+				if _, ok := token.Method.(*jwt.SigningMethodECDSA); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+				}
+				return k, nil
+			})
+		}
+	}
 	if err != nil {
 		return nil, err
 	}
@@ -32,18 +62,34 @@ func ValidateToken(tokenString string, key *rsa.PublicKey) (Claims, error) {
 	return nil, errors.New("invalid token")
 }
 
-func LoadPublicKey(keyFile string) (*rsa.PublicKey, error) {
+func LoadPublicKey(keyFile string) (interface{}, error) {
 	bs, err := ioutil.ReadFile(keyFile)
 	if err != nil {
 		return nil, err
 	}
-	return jwt.ParseRSAPublicKeyFromPEM(bs)
+	rsaKey, err := jwt.ParseRSAPublicKeyFromPEM(bs)
+	if err != nil {
+		ecKey, err := jwt.ParseECPublicKeyFromPEM(bs)
+		if err != nil {
+			return nil, errors.New("unknown public key type")
+		}
+		return ecKey, nil
+	}
+	return rsaKey, nil
 }
 
-func LoadPrivateKey(keyFile string) (*rsa.PrivateKey, error) {
+func LoadPrivateKey(keyFile string) (interface{}, error) {
 	bs, err := ioutil.ReadFile(keyFile)
 	if err != nil {
 		return nil, err
 	}
-	return jwt.ParseRSAPrivateKeyFromPEM(bs)
+	rsaKey, err := jwt.ParseRSAPrivateKeyFromPEM(bs)
+	if err != nil {
+		ecKey, err := jwt.ParseECPrivateKeyFromPEM(bs)
+		if err != nil {
+			return nil, errors.New("unknown public key type")
+		}
+		return ecKey, nil
+	}
+	return rsaKey, nil
 }
